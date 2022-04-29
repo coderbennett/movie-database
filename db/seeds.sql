26eea7a235b8aa2cc87d565e1db1bf86de27fadf
Snehita Kolli <snehitakolli20@gmail.com>
2022-04-29 13:25:01 -0700

    finish seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO movies (movies_name)
+VALUES  ("Fresh, the Movie"),
+        ("Shrek"), 
+        ("Harry Potter"),
+        ("Marvel"),
+        ("Titanic");
+
+INSERT INTO reviews (movie_id, review)
+VALUES  (1, "Weird, but really interesting."),
+        (2, "Classic and funny!"),
+        (3, "Welcome to the wizarding world!"),
+        (4, "This is Captain Marvel"), 
+        (5, "Very sad, my heart will go on.");
